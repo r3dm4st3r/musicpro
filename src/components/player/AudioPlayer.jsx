224f15ce91f73b4703b90b273aef35607738a415
r3dm4st3r <prafullaondemand@gmail.com>
2022-12-03 14:21:41 +0530

    initial commit

--- src/components/player/AudioPlayer.jsx
+++ src/components/player/AudioPlayer.jsx
@@ -1,15 +1,15 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 import { useAudioPlayer } from 'react-use-audio-player';
+import { useUpdateEffect } from 'react-haiku';
 import SeekBar from './controls/SeekBar';
 import SongDuration from './controls/SongDuration';
-import { useUpdateEffect } from 'react-haiku';
 import Play from './controls/Play';
 import Next from './controls/Next';
 import Prev from './controls/Prev';
 import Vol from './controls/Vol';
 import List from './controls/List';
 import Repeat from './controls/Repeat';
-import { useState } from 'react';
 
 const AudioPlayer = (props) => {
 	const { audio } = props;
@@ -36,40 +36,45 @@ const AudioPlayer = (props) => {
 	}, [repeated]);
 
 	if (!ready && !loading) return <div>No audio to play</div>;
-	if (loading) return <div>Loading audio</div>;
 
 	return (
-		<div className="bg-white dark:bg-darkOne dark:text-white relative z-[10]">
-			<SeekBar />
-			<div className="flex items-center justify-between px-2 py-2">
-				<div className="w-full flex items-center gap-3">
-					<div className="w-[50px] h-[50px] flex items-center justify-center">
-						<img className="shadow drop-shadow rounded" src={audio.thumb} alt="test" />
+		<>
+			<div className="bg-white dark:bg-darkOne dark:text-white relative z-[10]">
+				<SeekBar />
+				<div className="flex items-center justify-between px-2 py-2">
+					<div className="w-full flex items-center gap-3">
+						<div className="w-[50px] h-[50px] flex items-center justify-center">
+							<img
+								className="shadow drop-shadow rounded"
+								src={audio.thumb}
+								alt="test"
+							/>
+						</div>
+						<div className="w-[calc(100%-50px)] overflow-hidden">
+							<p className="text-[14px] mb-1 font-bold font-head">
+								Manike - O lala re o lala re
+							</p>
+							<SongDuration ready={ready} />
+						</div>
 					</div>
-					<div className="w-[calc(100%-50px)] overflow-hidden">
-						<p className="text-[14px] mb-1 font-bold font-head">
-							Manike - O lala re o lala re
-						</p>
-						<SongDuration ready={ready} />
+					<div className="w-full flex items-center justify-center gap-3">
+						<Prev />
+						<Play
+							ready={ready}
+							loading={loading}
+							playing={playing}
+							togglePlayPause={togglePlayPause}
+						/>
+						<Next />
 					</div>
-				</div>
-				<div className="w-full flex items-center justify-center gap-3">
-					<Prev />
-					<Play playing={playing} togglePlayPause={togglePlayPause} />
-					<Next />
-				</div>
-				<div className="w-full flex items-center justify-end">
-					<div className="font-extrabold mr-2">
-						<span className="text-[10px] block text-warning/70 border border-warning/30 px-2 py-0.5">
-							HD
-						</span>
+					<div className="w-full flex items-center justify-end">
+						<Vol mute={mute} />
+						<Repeat repeated={repeated} handleRepeat={handleRepeat} />
+						<List />
 					</div>
-					<Vol mute={mute} />
-					<Repeat repeated={repeated} handleRepeat={handleRepeat} />
-					<List />
 				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
