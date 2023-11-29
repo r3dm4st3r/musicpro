880d51a452bb393b0b6baaef198114d241b2da65
r3dm4st3r <prafullaondemand@gmail.com>
2023-11-30 02:00:43 +0530

    Logo link added

--- components/common/header/Header.tsx
+++ components/common/header/Header.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { Paper, Text } from "@mantine/core";
 import dynamic from "next/dynamic";
+import Link from "next/link";
 
 const LanguagePicker = dynamic(
   () => import("@/components/common/header/language/LanguagePicker"),
@@ -12,7 +13,10 @@ const Header: FC = () => {
       display="flex"
       className="py-3 px-5 flex items-center justify-between h-full"
     >
-      <div className="relative inline-flex">
+      <Link
+        href="/"
+        className="relative inline-flex text-transparent no-underline"
+      >
         <Text
           component="div"
           size="40px"
@@ -22,10 +26,10 @@ const Header: FC = () => {
         >
           MUSIC
         </Text>
-        <span className="font-extrabold absolute top-[-5px] right-[-40px]">
+        <span className="font-extrabold absolute top-[-5px] right-[-40px] text-[var(--mantine-primary-color-filled)]">
           PRO
         </span>
-      </div>
+      </Link>
       <div>
         <div>
           <LanguagePicker />
